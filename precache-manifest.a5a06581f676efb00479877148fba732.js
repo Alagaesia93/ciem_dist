95438bfddf3ee928433ad85e23fa1e161cdfbc87
alagaesia93 <ale.macagno@hotmail.it>
2019-06-23 17:10:45 +0200

    deploy

---
+++ precache-manifest.a5a06581f676efb00479877148fba732.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "27ab052c9e3d79db8251",
+    "url": "/ciem/css/app.a20cf4d5.css"
+  },
+  {
+    "revision": "27ab052c9e3d79db8251",
+    "url": "/ciem/js/app.16cbba68.js"
+  },
+  {
+    "revision": "b40e6e6f33d2bdd0d25e",
+    "url": "/ciem/js/chunk-vendors.5b542a54.js"
+  },
+  {
+    "revision": "fcb54f811ff81cc821d6",
+    "url": "/ciem/js/demo.712bca8d.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/ciem/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/ciem/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/ciem/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/ciem/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/ciem/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "6797c100bef08ca43df6dfdcb4a4fc4b",
+    "url": "/ciem/index.html"
+  },
+  {
+    "revision": "e0ea3f1916671872498288dc2bd40f06",
+    "url": "/ciem/img/brand/white.png"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/ciem/img/brand/favicon.png"
+  },
+  {
+    "revision": "59d7c2b119ab46fc7b5f853f04324c89",
+    "url": "/ciem/img/brand/green.png"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "/ciem/img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "/ciem/img/theme/angular.jpg"
+  },
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "/ciem/img/theme/react.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "/ciem/img/theme/sketch.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/ciem/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/ciem/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "/ciem/img/theme/vue.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ciem/robots.txt"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/ciem/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/ciem/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "/ciem/img/theme/profile-cover.jpg"
+  }
+];
